e157633cc9feec0d550bd3874c91a205716ddd85
Travel1999 <bobrovdmitry01@mail.ru>
2023-03-23 15:45:50 +0300

    hm-2

---
+++ hw-2.js
@@ -0,0 +1,68 @@
+// let a = 10; 
+// alert(a);
+
+// let a = 20;
+// alert(a);
+
+
+let iphoneYear = 2007;
+alert(iphoneYear);
+
+
+let javaScriptCreator = 'Брендан Эйх';
+alert(javaScriptCreator);
+
+
+let numberOne = 10;
+let numberTwo = 2;
+alert(numberOne + numberTwo);
+alert(numberOne - numberTwo);
+alert(numberOne * numberTwo);
+alert(numberOne / numberTwo);
+
+
+let result = (2**5);
+alert(result);
+
+
+let a = 9;
+let b = 2;
+newResult = 9%2;
+alert (newResult);
+
+
+let num = 1;
+num += 5;
+num -= 3;
+num *= 7;
+num /= 3;
+num += 1;
+num -= 1;
+alert(num);
+
+
+let age = Number(prompt('Сколько вам лет?'));
+alert(`Вам: ${age}`);
+
+let user = {
+	name: 'Dmitry',
+	age: 23,
+	isAdmin: false,
+}
+
+
+user['city of residence'] = 'Moscow';
+
+
+user.age = 24;
+
+
+delete user['city of residence'];
+
+
+let info = prompt('Какую информацию хотите узнать о пользователе?');
+alert(user.name)
+
+
+let UserName = prompt('Ваше имя')
+alert(`Привет, ${UserName}!`)
