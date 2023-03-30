bacd6450a5ca4747663b8ad66a9e0fb8a0ae7295
Travel1999 <bobrovdmitry01@mail.ru>
2023-03-30 16:42:35 +0300

    hm-4

---
+++ hw-4.js
@@ -0,0 +1,42 @@
+let i = 0;
+while (i < 2) {
+	console.log('Привет');
+	i++;
+}
+
+// let i = 1;
+// while (i < 6) {
+// 	console.log(i);
+// 	i++;
+// }
+
+// let i = 7;
+// while (i < 23) {
+// 	console.log(i);
+// 	i++
+// }
+
+// let obj = {
+// 	Коля: 200,
+// 	Вася: 300,
+// 	Петя: 400,
+// };
+// for (let name in obj) {
+// 	console.log(`${name} — зарплата ${obj[name]} долларов`);
+// }
+
+// for (let n = 1000; n >=50; ) {
+// 	n /= 2
+// 	if (n < 50) {
+// 		console.log(n);
+// 	}
+// }
+
+
+// for (let friday = 3, week = 0; friday < 32; week++) {
+	
+// 	if (week <= 5) {
+// 		console.log(`Сегодня пятница, ${friday}-е число. Необходимо подготовить отчет.`);
+// 	}
+// 	friday += 7;
+// }
