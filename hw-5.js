203b1c1773601bdd8ab33d66a8db4b3e872d7580
Travel1999 <bobrovdmitry01@mail.ru>
2023-04-04 10:39:28 +0300

    hm-5

--- hw-5.js
+++ hw-5.js
@@ -1,22 +1,22 @@
 // function calculate(a, b) { //Задание 1
-// 	if (a < b || b > a) {
+// 	if (a < b) {
 // 		return a;
 // 	} else {
 // 		return b;
 // 	}
 // }
 
-// console.log(calculate(100, 10));
+// console.log(calculate(4, 4));
 
-// function calculate (number) { //Задание 2
+// function calculate(number) { //Задание 2
 // 	if (number % 2 == 0) {
-// 		console.log('Число четное')
+// 		return 'Число четное';
 // 	} else {
-// 		console.log('Число нечетное');
+// 		return 'Число нечетное';
 // 	}
 // }
 
-// calculate(999)
+// console.log(calculate(100));
 
 // function toSquare (n) { //Задание 3.1
 // 	console.log(n**2)
@@ -45,24 +45,24 @@
 
 // function counter(a, b) {	//Задание 5
 // 	if (isNaN(a) || isNaN(b)) {
-// 		return console.log('Одно или оба значения не являются числом');
+// 		return 'Одно или оба значения не являются числом';
 // 	} else {
-// 		return console.log(a * b);
+// 		return a * b;
 // 	}
 // }
 
-// counter(3, 'Не число');
+// console.log(counter(3, 'Не число'));
 
 // function toAsk () { //Задание 6
 // 	let n = prompt('Введите число')
 // 	if (isNaN(n)) {
-// 		return console.log('Переданный параметр не является числом');
+// 		return 'Переданный параметр не является числом';
 // 	} else {
-// 		return console.log(`${n} в кубе равняется ${n ** 3}`);
+// 		return `${n} в кубе равняется ${n ** 3}`;
 // 	}
 // }
 
-// toAsk()
+// console.log(toAsk());
 
 // function getArea() { //Задание 7
 // 	return Math.PI * this.radius ** 2;
@@ -89,17 +89,18 @@
 // console.log(circle1.getPerimeter());
 // console.log(circle2.getPerimeter());
 
-// function seasonCalc() { // Задание 8
-// 	let month = Number(prompt('Введите число'));
-// 	if (month === 12 || month === 1 || month === 2) {
-// 		return console.log('Зима');
-// 	} else if (month === 3 || month === 4 || month === 5) {
-// 		return console.log('Весна');
-// 	} else if (month === 6 || month === 7 || month === 8) {
-// 		return console.log('Лето');
-// 	} else if (month === 9 || month === 10 || month === 11) {
-// 		return console.log('Осень');
-// 	} else {
-// 		return console.log('Не номер месяца');
-// 	}
-// }
+function seasonCalc() {
+	// Задание 8
+	let month = Number(prompt('Введите число'));
+	if (month === 12 || month === 1 || month === 2) {
+		return 'Зима';
+	} else if (month === 3 || month === 4 || month === 5) {
+		return 'Весна';
+	} else if (month === 6 || month === 7 || month === 8) {
+		return 'Лето';
+	} else if (month === 9 || month === 10 || month === 11) {
+		return 'Осень';
+	} else {
+		return 'Не номер месяца';
+	}
+}
